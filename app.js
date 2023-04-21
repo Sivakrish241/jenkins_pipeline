d1e7fb126050f4566ce0eec34dee093191d0902d
Sivakrish241 <111227468+Sivakrish241@users.noreply.github.com>
2023-04-21 11:35:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(' HI YOTTA TEAM...! WELCOME TO BROWN BAG SESSION !\n');
+  res.end(' It is a wonderful achivement  !\n');
 });
 
 server.listen(port, hostname, () => {
